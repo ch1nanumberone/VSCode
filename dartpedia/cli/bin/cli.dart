012a4a78b192022ca843f7f167f0aacff31ed3ff
superman <hemingw@yandex.ru>
2026-03-26 13:17:17 +0300

    константаа

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,4 +1,4 @@
-import 'package:cli/cli.dart' as cli; // Delete this entire line
+const version = '0.0.1'; // Add this line
 
 void main(List<String> arguments) {
   print('Hello, Dart!'); // Change this line
